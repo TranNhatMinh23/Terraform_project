ea7405c7bd98d568ad8297584ef63335432b058b
Tran Nhat Minh <trannhatminh23082001@gmail.com>
2023-05-03 16:51:08 +0700

    commit

---
+++ Chilashop/email_info.py
@@ -0,0 +1,7 @@
+from .email_info import *
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'trannhatminh23082001@gmail.com'
+EMAIL_HOST_PASSWORD = 'rwmqaxzilkikqfam'
+EMAIL_PORT = 587
